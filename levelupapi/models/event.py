ee237f831ee38a3283bf2cfaa35b4a525c49f559
Amanda De Leon <mandapanda6991@yahoo.com>
2022-11-03 14:56:37 -0500

    first commit

---
+++ levelupapi/models/event.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Event(models.Model):
+    game = models.ForeignKey("Game", on_delete=models.CASCADE)
+    description = models.CharField(max_length=155)
+    date = models.DateField()
+    time = models.TimeField()
+    organizer = models.ForeignKey("Gamer", on_delete=models.CASCADE)
